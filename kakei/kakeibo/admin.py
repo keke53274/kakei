9bc95aaedfcfb03b295ef4cc84e126edc26b98a3
usr1 <usr1@example.com>
2019-12-03 23:34:30 +0900

    Create a model for the kakeibo app, Insert data into kakeidb, kakeibo app display on admin site

--- kakei/kakeibo/admin.py
+++ kakei/kakeibo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Expense
 # Register your models here.
+
+#adminﾍﾟｰｼﾞを開いた際にkakeiboｱﾌﾟﾘが表示されるようにadminに伝える
+admin.site.register(Expense)
--- kakei/kakeibo/models.py
+++ kakei/kakeibo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,3 +9,11 @@ class Expense(models.Model):
     expense = models.BigIntegerField(default=0)
     expense_date = models.DateTimeField('date published')
     expense_detail = models.CharField(max_length=200)
+    #「Expense.objects.all()」をした時、結果の「<QuerySet [<Expense:☆☆☆ >]>」の「☆」の部分になる
+    def __str__(self):
+        #return self.expense_detail
+        return self.expense_user
+
+    #年月日で公開された質問を入手
+    def was_published_recently(self):
+        return self.expense_date >= timezone.now() - datetime.timedelta(days=1)
