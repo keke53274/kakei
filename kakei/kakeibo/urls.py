361420b864683fb2b8c7136f0a9da677489d59eb
usr1 <usr1@example.com>
2019-12-04 23:55:05 +0900

    kakeibo's view create and render shurtcuts

--- kakei/kakeibo/urls.py
+++ kakei/kakeibo/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:expense_id>/', views.detail, name='detail'),
+    path('<int:expense_id>/results/', views.results, name='results'),
+    path('<int:expense_id>/expense/', views.expense, name='expense'),
+    
 ]
--- kakei/kakeibo/views.py
+++ kakei/kakeibo/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
+from .models import Expense
 
+#render関数版( loader や HttpResponse を import する必要がなくなる)
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_expense_list = Expense.objects.order_by('-expense_date')[:5]
+    context = {'latest_expense_list' : latest_expense_list}
+    #renderfunction in request, templateload, context
+    return render(request, 'kakeibo/index.html', context)
+
+#not renderfunction
+#def index(request):
+#    latest_expense_list = Expense.objects.order_by('-expense_date')[:5]
+#    template = loader.get_template('kakeibo/index.html')
+#    context = {'latest_expense_list' : latest_expense_list,}
+#    return HttpResponse(template.render(context,request))
+
+def detail(request, expense_id):
+    return HttpResponse("you looking expense %s" % expense_id)    
+
+def results(request, expense_id):
+    response = "you looking results %s"
+    return HttpResponse(response % expense_id)
+
+def expense(rewuest, expense_id):
+    return HttpResponse("you expense %s" % expense_id)
