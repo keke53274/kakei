cc2ef49593191dc46a720c197348694d96f056b4
usr1 <usr1@example.com>
2020-02-17 23:55:16 +0900

    ﾌｫｰﾑ変更

--- kakei/kakeibo/forms.py
+++ kakei/kakeibo/forms.py
@@ -1,8 +1,23 @@
 from django import forms
 from .models import Expense
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import ModelForm
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
 
 class ExpenseAdd(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ["expense_user", "expense", "expense_date", "expense_time", "expense_detail"]
+        fields = '__all__'
+        #fields = ["expense_user", "expense", "expense_date", "expense_time", "expense_detail"]
+        widgets = {
+            #"expense_date" : AdminDateWidget(),
+            'expense_date' : DateInput(),
+            'expense_time' : TimeInput(),
+        }
+
 
